24d6d2c43eb06d9c3ca557bf4229a73e53372b00
Kåre Mølgaard Rasmussen <34368915+dk-moelgaard75@users.noreply.github.com>
2019-03-14 23:02:05 +0100

    Fremdrift i TicTacTor løsningen

---
+++ web/backend/uge3/TicTacToe2/TicTacToe2/TicTacToe2/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe2.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public System.DateTime? EmailConfirmationDate { get; set; }
+        public int Score { get; set; }
+
+    }
+}
