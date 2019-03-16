ac1288cba0091c3857c981a199ac2f2d970e24a4
Kåre Mølgaard Rasmussen <34368915+dk-moelgaard75@users.noreply.github.com>
2019-03-16 15:58:32 +0100

    UserRegistration - sidste del

--- web/backend/uge3/TicTacToe2/TicTacToe2/TicTacToe2/Models/UserModel.cs
+++ web/backend/uge3/TicTacToe2/TicTacToe2/TicTacToe2/Models/UserModel.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TicTacToe2.Models
 {
     public class UserModel
     {
         public Guid Id { get; set; }
+        [Required()]
         public String FirstName { get; set; }
+        [Required()]
         public String LastName { get; set; }
+        [Required(), DataType(DataType.EmailAddress)]
         public String Email { get; set; }
+        [Required(), DataType(DataType.Password)]
         public String Password { get; set; }
+        
         public bool IsEmailConfirmed { get; set; }
         public System.DateTime? EmailConfirmationDate { get; set; }
-        public int Score { get; set; }
+        public int? Score { get; set; }
 
     }
 }
--- web/backend/uge3/TicTacToe2/TicTacToe2/TicTacToe2/Startup.cs
+++ web/backend/uge3/TicTacToe2/TicTacToe2/TicTacToe2/Startup.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using TicTacToe2.Extensions;
@@ -16,7 +18,10 @@ namespace TicTacToe2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //Tillader directory browsing
+            //services.AddDirectoryBrowser();
             services.AddSingleton<Services.IUserServices, Services.UserServices>();
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,13 +37,41 @@ namespace TicTacToe2
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseStaticFiles();
+            RouteBuilder routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("CreateUser", context =>
+            {
+                var firstName = context.Request.Query["firstname"];
+                var lastName = context.Request.Query["lastname"];
+                var passWord = context.Request.Query["password"];
+                var email = context.Request.Query["email"];
+                var userService = context.RequestServices.GetService<Services.IUserServices>();
+                userService.RegisterUser(new Models.UserModel { FirstName = firstName, LastName = lastName, Email = email, Password = passWord });
+                return context.Response.WriteAsync($"User {firstName} {lastName} has been succesfully created - thru routing");
+
+            });
+            routeBuilder.MapGet("DoTest", context =>
+            {
+                var fn = context.Request.Query["fn"];
+                var ln = context.Request.Query["ln"];
+                return context.Response.WriteAsync($"DoTest called with {fn} {ln}");
+            }
+            );
+            
+            var newUserRoutes = routeBuilder.Build();
+            app.UseRouter(newUserRoutes);
+            var options = new RewriteOptions();
+            options.AddRewrite("NewUser", "UserRegistration/Index", false);
+            app.UseRewriter(options);
             app.UseCommunicationMiddleware();
+            //Laver en directorylistning
+            //app.UseDirectoryBrowser();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                       name: "default",
                       template: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseStatusCodePages("text/plain", "HTTP Error - Status code: {0}");
         }
     }
 }
